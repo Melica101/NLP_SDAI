3b8d3a616533c57921e8d818d8d0fe597caa636f
Melica101 <amalicamelly@gmail.com>
2025-06-14 11:52:27 +0200

    prolog: added terms

--- prolog_module/fact_checker.pl
+++ prolog_module/fact_checker.pl
@@ -1,8 +1,36 @@
-:- module(fact_checker, [is_fake/1]).
+:- module(fact_checker, [is_fake/1, is_real/1]).
 
-is_fake(Text) :- contains(Text, "flat earth").
-is_fake(Text) :- contains(Text, "alien").
-is_fake(Text) :- contains(Text, "cure for cancer").
-is_fake(Text) :- contains(Text, "conspiracy").
+fake_news_terms([
+    "flat earth",
+    "alien",
+    "UFO",
+    "ghost",
+    "cure for cancer",
+    "miracle",
+    "conspiracy",
+    "scandal",
+    "secret", "sex",
+    "fake", "conspiracy", "hoax", "scam", "fraud"
+]).
+
+real_news_terms([
+    "CNN",
+    "BBC News",
+    "Reuters",
+    "Associated Press",
+    "CNN Report", "Fox News Alert",
+    "rule", "policy", "regulation", "law",
+    "government", "election", "vote"
+]).
+
+is_fake(Text) :-
+    fake_news_terms(FakeTerms),
+    member(Sub, FakeTerms),
+    contains(Text, Sub).
+
+is_real(Text) :-
+    real_news_terms(RealTerms),
+    member(Sub, RealTerms),
+    contains(Text, Sub).
 
 contains(Text, Sub) :- sub_string(Text, _, _, _, Sub).
